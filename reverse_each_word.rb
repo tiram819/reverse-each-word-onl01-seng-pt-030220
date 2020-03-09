a03ed40810e6798be57a6679cd332f18b4c012a8
Tabassum Hassan <tiram819@gmail.com>
2020-03-09 04:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,8 @@ def reverse_each_word(array)
   return reversed_array.join(" ")
 end
 
+def reverse_each_word(array)
+  method = array.split(" ")
+  method.collect {|string| string.reverse!}
+  method.join(" ")
+end
